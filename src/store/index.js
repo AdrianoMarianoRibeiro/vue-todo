e5430bcee3d28ff37a5938a2aeec4dce341cf8a7
Adriano <adrianomribeiro1@gmail.com>
2024-05-10 12:22:51 -0300

    feat(master): todo :sparkles: - Lista de tarefas sem vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+      {
+        titulo: 'Ir ao mercado',
+        concluido: false
+      },
+      {
+        titulo: 'Comprar ração',
+        concluido: false
+      },
+      {
+        titulo: 'Jogar bola',
+        concluido: false
+      }
+    ]
   },
   getters: {
   },
